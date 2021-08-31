6c67fc7bbea059cf145a72706998083871ff0456
jakemor <jakemny@gmail.com>
2021-08-31 14:25:50 -0700

    fixes init in debug view controller to work with host apps that use storyboards

--- Sources/Paywall/Debug/DebugViewController.swift
+++ Sources/Paywall/Debug/DebugViewController.swift
@@ -104,6 +104,15 @@ internal class DebugViewController: UIViewController {
     var paywallResponse: PaywallResponse? = nil
     var paywallResponses: [PaywallResponse] = []
     
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
