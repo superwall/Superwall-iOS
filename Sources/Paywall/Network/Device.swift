d034c3ee77133c0312efb8d28f41a8fdb0f53e72
jakemor <jakemny@gmail.com>
2021-09-09 12:36:21 -0400

    git checks swift package versioning

--- Sources/Paywall/Network/Device.swift
+++ Sources/Paywall/Network/Device.swift
@@ -72,6 +72,7 @@ extension UIDevice {
         return machineMirror.children.reduce("") { identifier, element in
             guard let value = element.value as? Int8, value != 0 else { return identifier }
             return identifier + String(UnicodeScalar(UInt8(value)))
+            
         }
     }
 }
