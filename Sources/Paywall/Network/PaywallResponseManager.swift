5632e988fa09c07eb73d636acecd0f536b7545c4
jakemor <jakemny@gmail.com>
2021-10-27 16:33:02 -0400

    Update PaywallResponseManager.swift

--- Sources/Paywall/Network/PaywallResponseManager.swift
+++ Sources/Paywall/Network/PaywallResponseManager.swift
@@ -33,10 +33,9 @@ class PaywallResponseManager: NSObject {
 		}
 		
 		// if the request is in progress, enque the completion handler
-		if handlersByHash[hash] != nil {
-//			handlersByHash[hash] = handlers + [completion] // uncomment to execute all completion handlers
-			// only last completion handler wins
-			handlersByHash[hash] = [completion]
+		if let handlers = handlersByHash[hash] {
+//			handlersByHash[hash] = handlers + [completion] // to execute all completion handlers
+			handlersByHash[hash] = [completion] // to only execute the last completion handler
 			return
 		}
 		
