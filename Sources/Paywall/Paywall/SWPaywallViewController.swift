a9b7c71420cb1586c67813d3d82fbd198ced9923
Jake <jakemor@users.noreply.github.com>
2022-02-15 16:59:20 +0000

    xcode 12 support

--- Sources/Paywall/Paywall/SWPaywallViewController.swift
+++ Sources/Paywall/Paywall/SWPaywallViewController.swift
@@ -99,7 +99,7 @@ internal class SWPaywallViewController: UIViewController {
 		let preferences = WKPreferences()
 		if #available(iOS 15.0, *) {
 			if !DeviceHelper.shared.isMac {
-				preferences.isTextInteractionEnabled = false // ignore-xcode-12
+// 				preferences.isTextInteractionEnabled = false // ignore-xcode-12
 			}
 		}
 		preferences.javaScriptCanOpenWindowsAutomatically = true
@@ -257,7 +257,7 @@ internal class SWPaywallViewController: UIViewController {
 		if isActive && !isSafariVCPresented {
 			if #available(iOS 15.0, *) {
 				if !DeviceHelper.shared.isMac {
-					webview.setAllMediaPlaybackSuspended(false, completionHandler: nil) // ignore-xcode-12
+// 					webview.setAllMediaPlaybackSuspended(false, completionHandler: nil) // ignore-xcode-12
 				}
 			}
 			
@@ -292,7 +292,7 @@ internal class SWPaywallViewController: UIViewController {
 			}
 			if #available(iOS 15.0, *) {
 				if !DeviceHelper.shared.isMac {
-					webview.setAllMediaPlaybackSuspended(true, completionHandler: nil) // ignore-xcode-12
+// 					webview.setAllMediaPlaybackSuspended(true, completionHandler: nil) // ignore-xcode-12
 				}
 			}
 
