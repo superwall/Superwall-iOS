004a14c41818e6bef081f3dc589a39085bcc1208
Brian Anglin <brianranglin@gmail.com>
2022-03-21 13:08:02 -0600

    Attempts to fix spacing

--- Tests/PaywallTests/Paywall/Trigger Manager/ExpressionEvaluatorTests.swift
+++ Tests/PaywallTests/Paywall/Trigger Manager/ExpressionEvaluatorTests.swift
@@ -14,7 +14,6 @@ final class ExpressionEvaluatorTests: XCTestCase {
     Storage.shared.clear()
   }
 
-  // TODO: Why is this failing?
 
   func testExpressionMatchesAll() {
     let result = ExpressionEvaluator.evaluateExpression(
@@ -33,32 +32,32 @@ final class ExpressionEvaluatorTests: XCTestCase {
     XCTAssertTrue(result)
   }
 
-    func testExpressionEvaluatorParams() {
-        Storage.shared.userAttributes = [:]
-      let result = ExpressionEvaluator.evaluateExpression(
-        expression: "params.a == \"b\"",
-        eventData: EventData(name: "ss", parameters: ["a": "b"], createdAt: "")
-      )
-      XCTAssertTrue(result)
-    }
+  func testExpressionEvaluatorParams() {
+    Storage.shared.userAttributes = [:]
+    let result = ExpressionEvaluator.evaluateExpression(
+      expression: "params.a == \"b\"",
+      eventData: EventData(name: "ss", parameters: ["a": "b"], createdAt: "")
+    )
+    XCTAssertTrue(result)
+  }
 
-    func testExpressionEvaluatorDeviceTrue() {
-        Storage.shared.userAttributes = [:]
-      let result = ExpressionEvaluator.evaluateExpression(
-        expression: "device.platform == \"iOS\"",
-        eventData: EventData(name: "ss", parameters: ["a": "b"], createdAt: "")
-      )
-      XCTAssertTrue(result)
-    }
+  func testExpressionEvaluatorDeviceTrue() {
+    Storage.shared.userAttributes = [:]
+    let result = ExpressionEvaluator.evaluateExpression(
+      expression: "device.platform == \"iOS\"",
+      eventData: EventData(name: "ss", parameters: ["a": "b"], createdAt: "")
+    )
+    XCTAssertTrue(result)
+  }
 
-    func testExpressionEvaluatorDeviceFalse() {
-        Storage.shared.userAttributes = [:]
-      let result = ExpressionEvaluator.evaluateExpression(
-        expression: "device.platform == \"Android\"",
-        eventData: EventData(name: "ss", parameters: ["a": "b"], createdAt: "")
-      )
-      XCTAssertFalse(result)
-    }
+  func testExpressionEvaluatorDeviceFalse() {
+    Storage.shared.userAttributes = [:]
+    let result = ExpressionEvaluator.evaluateExpression(
+      expression: "device.platform == \"Android\"",
+      eventData: EventData(name: "ss", parameters: ["a": "b"], createdAt: "")
+    )
+    XCTAssertFalse(result)
+  }
 
   func testExpressionEvaluatorFalse() {
     Storage.shared.userAttributes = [:]
