e1a8c22d9c129810a66afdfb8a1b113bfb8f164c
Yusuf Tör <yusuf@yusuftor.co.uk>
2022-06-23 12:09:12 +0100

    Prevented background requests to config
    - If config is called in the background, the request is queued for when the app is active.
    - Added exponential backoff for URLSession request retry. Goes from a 5 second delay to a 25 second delay.

---
+++ Sources/Paywall/Models/Config/ConfigRequest.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yusuf Tör on 22/06/2022.
+//
+
+import Foundation
+
+struct ConfigRequest: Equatable {
+  let id: String
+  let completion: ((Result<Config, Error>) -> Void)
+  
+  static func == (lhs: ConfigRequest, rhs: ConfigRequest) -> Bool {
+    return lhs.id == rhs.id
+  }
+}
