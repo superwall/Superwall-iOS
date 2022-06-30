65d70fb31e19281da1b1e48a245af0b75804e65a
Jake <jakemor@users.noreply.github.com>
2022-06-30 23:58:02 +0000

    xcode 12 support

--- Sources/Paywall/Paywall/Paywall View Controller/SWPaywallViewController.swift
+++ Sources/Paywall/Paywall/Paywall View Controller/SWPaywallViewController.swift
@@ -225,7 +225,7 @@ final class SWPaywallViewController: UIViewController, SWWebViewDelegate {
 
     if #available(iOS 15.0, *),
       !DeviceHelper.shared.isMac {
-      webView.setAllMediaPlaybackSuspended(false) // ignore-xcode-12
+//       webView.setAllMediaPlaybackSuspended(false) // ignore-xcode-12
     }
 
     if UIWindow.isLandscape {
@@ -272,7 +272,7 @@ final class SWPaywallViewController: UIViewController, SWWebViewDelegate {
     }
     if #available(iOS 15.0, *),
       !DeviceHelper.shared.isMac {
-      webView.setAllMediaPlaybackSuspended(true) // ignore-xcode-12
+//       webView.setAllMediaPlaybackSuspended(true) // ignore-xcode-12
     }
 
     if !calledDismiss {
