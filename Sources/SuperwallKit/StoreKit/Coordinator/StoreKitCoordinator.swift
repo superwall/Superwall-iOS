6e520e06ce16067fd31604bd3c7a95ee3dfeea5b
Yusuf Tör <yusuf@yusuftor.co.uk>
2023-01-06 18:40:58 +0000

    Update StoreKitCoordinator.swift

--- Sources/SuperwallKit/StoreKit/Coordinator/StoreKitCoordinator.swift
+++ Sources/SuperwallKit/StoreKit/Coordinator/StoreKitCoordinator.swift
@@ -7,18 +7,8 @@
 
 import Foundation
 
-/// Coordinates: The purchasing, restoring and retrieving of products; the checking
+/// Coordinates the purchasing, restoring and retrieving of products; the checking
 /// of transactions; and the determining of the user's subscription status.
-///
-/// We can't use StoreKit 2 because:
-/// 1. If there are unfinished StoreKit transaction updates on app open, only the first
-///    listener is given the updates. This means that our listener can interfere with
-///    the clients listener (if they're using SK2) and their transactions may not even show up!
-/// 2. If the developer used an SK1 listener, their listener could finish the txns before
-///    ours. Which means that we will never see the unfinished transactions or we'll see only
-///    a few of them.
-/// 3. The Transaction.updates listener is not reliably called for transactions made outside
-///   the app. Therefore the transactions aren't always finished.
 struct StoreKitCoordinator {
   /// Purchases the product.
   let productPurchaser: ProductPurchaser
@@ -32,9 +22,6 @@ struct StoreKitCoordinator {
   /// Restores purchases.
   let txnRestorer: TransactionRestorer
 
-  // Using unowned here because we will always have a subscriptionStatusHandler
-  // but since this is sometimes the class that created the coordinator, we
-  // don't want to create a strong reference cycle.
   /// Checks if the user is subscribed.
   unowned let subscriptionStatusHandler: SubscriptionStatusChecker
 
@@ -45,17 +32,17 @@ struct StoreKitCoordinator {
   ) {
     self.productFetcher = ProductsFetcherSK1()
 
-    let hasDelegate = delegateAdapter.hasDelegate
     let sk1ProductPurchaser = factory.makeSK1ProductPurchaser()
+    self.txnChecker = sk1ProductPurchaser
+
+    let hasDelegate = delegateAdapter.hasDelegate
 
     if hasDelegate {
       self.productPurchaser = delegateAdapter
-      self.txnChecker = sk1ProductPurchaser
       self.txnRestorer = delegateAdapter
       self.subscriptionStatusHandler = delegateAdapter
     } else {
       self.productPurchaser = sk1ProductPurchaser
-      self.txnChecker = sk1ProductPurchaser
       self.txnRestorer = sk1ProductPurchaser
       self.subscriptionStatusHandler = storeKitManager
     }
