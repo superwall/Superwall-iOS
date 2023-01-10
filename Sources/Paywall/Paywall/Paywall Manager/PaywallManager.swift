3f889027f58fe7ee03199df8b72a75c23a7b68d7
Yusuf Tör <yusuf@yusuftor.co.uk>
2023-01-10 11:58:25 +0000

    Added missing items for paywall response substitution

--- Sources/Paywall/Paywall/Paywall Manager/PaywallManager.swift
+++ Sources/Paywall/Paywall/Paywall Manager/PaywallManager.swift
@@ -64,8 +64,9 @@ final class PaywallManager {
           // Set product-related vars again incase products have been substituted into paywall.
           viewController.paywallResponse.products = response.products
           viewController.paywallResponse.swProducts = response.swProducts
-          viewController.paywallResponse.isFreeTrialAvailable = response.isFreeTrialAvailable
+          viewController.paywallResponse.variables = response.variables
           viewController.paywallResponse.productVariables = response.productVariables
+          viewController.paywallResponse.isFreeTrialAvailable = response.isFreeTrialAvailable
           viewController.paywallResponse.productsLoadFailTime = response.productsLoadFailTime
           viewController.paywallResponse.productsLoadStartTime = response.productsLoadStartTime
           viewController.paywallResponse.productsLoadCompleteTime = response.productsLoadCompleteTime
