80f8ff996e9866403d096759c34016621f6f329f
jakemor <jakemny@gmail.com>
2023-03-25 19:06:38 -0400

    adds .onPresent, .onDismiss and .onError convenience static functions to PaywallPresentationHandler for use with Superwall.shared.register

--- Sources/SuperwallKit/Paywall/Presentation/PaywallPresentationHandler.swift
+++ Sources/SuperwallKit/Paywall/Presentation/PaywallPresentationHandler.swift
@@ -18,4 +18,28 @@ public class PaywallPresentationHandler: NSObject {
 
   /// A block called when an error occurred while trying to present a paywall.
   public var onError: ((_ error: Error) -> Void)?
+
+//  init(onPresent: (@escaping (_: PaywallInfo) -> Void)? = nil, onDismiss: (@escaping (_: PaywallInfo) -> Void)? = nil, onError: (@escaping (_: Error) -> Void)? = nil) {
+//    self.onPresent = onPresent
+//    self.onDismiss = onDismiss
+//    self.onError = onError
+//  }
+
+  public static func onPresent(_ onPresent: ((_ paywallInfo: PaywallInfo) -> Void)?) -> PaywallPresentationHandler {
+    let h = PaywallPresentationHandler()
+    h.onPresent = onPresent
+    return h
+  }
+
+  public static func onDismiss(_ onDismiss: ((_ paywallInfo: PaywallInfo) -> Void)?) -> PaywallPresentationHandler {
+    let h = PaywallPresentationHandler()
+    h.onDismiss = onDismiss
+    return h
+  }
+
+  public static func onError(_ onError: ((_ error: Error) -> Void)?) -> PaywallPresentationHandler {
+    let h = PaywallPresentationHandler()
+    h.onError = onError
+    return h
+  }
 }
