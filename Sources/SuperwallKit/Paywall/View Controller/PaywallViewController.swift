78365327b5b85aea5ff51e533b1a650502e63ff4
Jake <jakemor@users.noreply.github.com>
2023-04-28 18:15:37 +0000

    xcode 12 support

--- Sources/SuperwallKit/Paywall/View Controller/PaywallViewController.swift
+++ Sources/SuperwallKit/Paywall/View Controller/PaywallViewController.swift
@@ -627,7 +627,7 @@ extension PaywallViewController {
 
     if #available(iOS 15.0, *),
       !deviceHelper.isMac {
-      webView.setAllMediaPlaybackSuspended(false) // ignore-xcode-12
+//       webView.setAllMediaPlaybackSuspended(false) // ignore-xcode-12
     }
   }
 
@@ -659,7 +659,7 @@ extension PaywallViewController {
 
     if #available(iOS 15.0, *),
       !deviceHelper.isMac {
-      webView.setAllMediaPlaybackSuspended(true) // ignore-xcode-12
+//       webView.setAllMediaPlaybackSuspended(true) // ignore-xcode-12
     }
 
     if !calledDismiss {
