b093d6d43824b023ea288ed57be000a698ce4936
Yusuf Tör <yusuf@yusuftor.co.uk>
2023-09-06 12:24:38 +0800

    Fix tests, update changelog

--- Tests/SuperwallKitTests/Config/Models/SurveyTests.swift
+++ Tests/SuperwallKitTests/Config/Models/SurveyTests.swift
@@ -27,7 +27,8 @@ final class SurveyTests: XCTestCase {
       message: "test",
       options: [.stub()],
       presentationProbability: 0,
-      includeOtherOption: true
+      includeOtherOption: true,
+      includeCloseButton: true
     )
     let isHoldout = survey.shouldAssignHoldout(
       isDebuggerLaunched: false,
@@ -44,7 +45,8 @@ final class SurveyTests: XCTestCase {
       message: "test",
       options: [.stub()],
       presentationProbability: 1,
-      includeOtherOption: true
+      includeOtherOption: true,
+      includeCloseButton: true
     )
     let isHoldout = survey.shouldAssignHoldout(
       isDebuggerLaunched: false,
@@ -61,7 +63,8 @@ final class SurveyTests: XCTestCase {
       message: "test",
       options: [.stub()],
       presentationProbability: 0.4,
-      includeOtherOption: true
+      includeOtherOption: true,
+      includeCloseButton: true
     )
     func random(in: Range<Double>) -> Double {
       return 0.5
@@ -89,7 +92,8 @@ final class SurveyTests: XCTestCase {
       message: "test",
       options: [.stub()],
       presentationProbability: 0.4,
-      includeOtherOption: true
+      includeOtherOption: true,
+      includeCloseButton: true
     )
     let existingAssignmentKey = "abc"
     let storage = StorageMock(internalSurveyAssignmentKey: existingAssignmentKey)
@@ -105,7 +109,8 @@ final class SurveyTests: XCTestCase {
       message: "test",
       options: [.stub()],
       presentationProbability: 0.4,
-      includeOtherOption: true
+      includeOtherOption: true,
+      includeCloseButton: true
     )
     let existingAssignmentKey = "abc"
     let storage = StorageMock(internalSurveyAssignmentKey: existingAssignmentKey)
