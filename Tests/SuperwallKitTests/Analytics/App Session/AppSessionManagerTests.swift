df4425b5ecadb6d22309e595e989ae55e5b857a7
Yusuf Tör <yusuf@yusuftor.co.uk>
2024-06-28 16:24:18 +0200

    Update AppSessionManagerTests.swift

--- Tests/SuperwallKitTests/Analytics/App Session/AppSessionManagerTests.swift
+++ Tests/SuperwallKitTests/Analytics/App Session/AppSessionManagerTests.swift
@@ -63,8 +63,8 @@ class AppSessionManagerTests: XCTestCase {
       Notification(name: UIApplication.didBecomeActiveNotification)
     )
 
-    try? await Task.sleep(nanoseconds: 10_000_000)
-    
+    try? await Task.sleep(nanoseconds: 50_000_000)
+
     XCTAssertNotEqual(appSessionManager.appSession.id, oldAppSession.id)
   }
 
