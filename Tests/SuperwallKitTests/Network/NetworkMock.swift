2dabd6db7cf0a1d6202dbdb41ae54c476ecb9a3b
Yusuf Tör <3296904+yusuftor@users.noreply.github.com>
2025-04-10 21:06:08 +0200

    Adds test to check timeout actually cancels

--- Tests/SuperwallKitTests/Network/NetworkMock.swift
+++ Tests/SuperwallKitTests/Network/NetworkMock.swift
@@ -24,7 +24,8 @@ final class NetworkMock: Network {
   override func getConfig(
     injectedApplicationStatePublisher: (AnyPublisher<UIApplication.State, Never>)? = nil,
     maxRetry: Int? = nil,
-    isRetryingCallback: ((Int) -> Void)? = nil
+    isRetryingCallback: ((Int) -> Void)? = nil,
+    timeout: Seconds? = nil
   ) async throws -> Config {
     getConfigCalled = true
 
