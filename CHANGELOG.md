2f230e7840a693be528bd807578a4bbc5064c41b
Yusuf Tör <3296904+yusuftor@users.noreply.github.com>
2025-05-27 15:17:13 +0200

    Update Superscript to 0.2.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 The changelog for `SuperwallKit`. Also see the [releases](https://github.com/superwall/Superwall-iOS/releases) on GitHub.
 
+## 4.4.2
+
+### Enhancements
+
+- Updates Superscript to 0.2.6.
+
 ## 4.4.1
 
 ### Fixes
--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/superwall/Superscript-iOS", .exact("0.2.4"))
+    .package(url: "https://github.com/superwall/Superscript-iOS", .exact("0.2.6"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
