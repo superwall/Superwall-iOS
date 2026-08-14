1a052cee509bd0bf324a1baf759e81626337675f
Yusuf Tör <3296904+yusuftor@users.noreply.github.com>
2026-08-14 17:12:12 +0200

    Update Superscript

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/superwall/superscript-ios-next", .exact("1.0.14"))
+    .package(url: "https://github.com/superwall/superscript-ios-next", .exact("1.0.15"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
